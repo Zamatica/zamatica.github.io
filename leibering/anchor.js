0b81f7ef94e5f0dc80ad9b308ed0c5f0724ebde9
Sweet <n.sweet@digipen.com>
2017-07-09 14:01:41 -0500

    Added leibering website to this site

---
+++ leibering/anchor.js
@@ -0,0 +1,44 @@
+
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 75);
+  }
+}
+
+// Captures click events of all a elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) 
+{
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() 
+  {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && 
+           location.hostname == this.hostname) 
+    {
+
+      var target = $(this.hash);
+
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+      if (target.length) 
+      {
+        $('html,body').animate(
+        {
+          scrollTop: target.offset().top - 75
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
